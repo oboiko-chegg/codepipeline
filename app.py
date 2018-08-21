9ee75a2c31b6e1ad162a3bf3932220dd380c77a6
Oleg Boiko <oboiko@chegg.com>
2018-08-21 00:23:11 -0700

    Adding timestamp

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import falcon
 import uuid
 import json
+from datetime import datetime
 
 
 class UuidResource:
     def on_get(self, req, resp):
-        body = {'uuid': str(uuid.uuid4())}
+        body = {'uuid': str(uuid.uuid4()),
+                'timestamp': str(datetime.now())}
         resp.media = body
 
 api = falcon.API()
